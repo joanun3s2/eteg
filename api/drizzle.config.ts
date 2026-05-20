68c525d07d83150a5f5f6c6c3d6788cc0eed96a9
joao nunes <joao.nunes@salesbuildr.com>
2026-05-20 20:28:30 -0300

    changed drizzle config for deploy

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,12 +1,9 @@
-import { env } from '@/env';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
-    user: env.DATABASE_USER,
-    password: env.DATABASE_PASSWORD,
+    url: process.env.DATABASE_URL!,
   },
   out: './src/db/migrations',
   schema: './src/db/schema/index.ts',
