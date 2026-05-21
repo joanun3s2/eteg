cb45375857a0f7c9ba51bb9020331c5957f82916
joao nunes <joao.nunes@salesbuildr.com>
2026-05-20 21:00:00 -0300

    add absolute path to drizzle config to deploy

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
+import path from 'path';
 
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-  out: './src/db/migrations',
-  schema: './src/db/schema/index.ts',
+  schema: path.resolve(process.cwd(), 'src/db/schema/index.ts'),
+  out: path.resolve(process.cwd(), 'src/db/migrations'),
   casing: 'camelCase',
 });
